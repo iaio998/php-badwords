a6f95a3d9ea3cd230c4931b707d041ce4023ad80
iaio998 <depalatisiacopo@gmail.com>
2023-11-27 16:00:44 +0100

    Added bonus

--- info.php
+++ info.php
@@ -1,21 +1,25 @@
 <?php
 $text = $_GET["testo"];
 $word = $_GET["parola"];
-strlen($text);
-str_replace($word, "###", $text);
-strlen(str_replace($word, "###", $text));
+$textlen = strlen($text);
+$replacestring = str_replace($word, "###", $text);
+$replacelen = strlen($replacestring);
+$array = explode(" ", $text);
+$arraylen = count($array);
 ?>
 
 
+
 <p class="fs-2">
-    <?php echo $text . "," . " " . " lunghezza stringa:" . strlen($text) ?>
+    <?php echo $text . "," . " " . " lunghezza stringa:" . $textlen ?>
+</p>
+<h5 class="fs-2">
+    <?php echo "Lunghezza caratteri: " . $textlen ?>
+    <?php echo "Totale parole: " . $arraylen ?>
+</h5>
+<p class="fs-2">
+    <?php echo $replacestring ?>
 </p>
 <p class="fs-2">
-    <?php echo $word ?>
-</p>
-<p class="fs-2">
-    <?php echo str_replace($word, "###", $text) ?>
-</p>
-<p class="fs-2">
-    <?php echo "Nuova lunghezza stringa:" . strlen(str_replace($word, "###", $text)) ?>
+    <?php echo "Nuova lunghezza stringa: " . $replacelen ?>
 </p>
